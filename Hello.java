21177b7f908977cc8567f314dee4aa7b431e0f78
ajpaucar1 <javierpaucar16@gmail.com>
2021-11-30 17:30:40 -0500

    Version1

---
+++ Hello.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+        public static void main(String [] args){
+                System.out.println("\nHello Objects Oriented Programming from Javier Paucar\n");
+        }
+}
