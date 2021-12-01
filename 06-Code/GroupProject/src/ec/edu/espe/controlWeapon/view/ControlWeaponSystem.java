f7d465be2325428a3b8bc6abbe5b6ab5d4425a37
lionarts16 <leoalexander16agos@gmail.com>
2021-11-30 19:43:54 -0500

    Project V1

---
+++ 06-Code/GroupProject/src/ec/edu/espe/controlWeapon/view/ControlWeaponSystem.java
@@ -0,0 +1,21 @@
+package ec.edu.espe.controlWeapon.view;
+
+import ec.edu.espe.controlWeapon.model.Ammunition;
+import ec.edu.espe.controlWeapon.model.WarMaterial;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ControlWeaponSystem {
+        public static void main(String[] args) {
+            System.out.println(" ------->Alexander Ruano<-----");
+            Ammunition ammunition= new Ammunition();
+            WarMaterial warMaterial=new WarMaterial();
+            System.out.println("Ammunition is ---> " + ammunition);
+            System.out.println("WarMaterial is ---> " + warMaterial);
+            
+           
+            
+        }
+}
